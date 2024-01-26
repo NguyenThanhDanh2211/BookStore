dd1e73f19fe8c1ea67810b5f1ef4c7e0e8795137
Nguyen Thanh Danh <nguyenthanhdanh221102@gmail.com>
2024-01-26 19:36:26 +0700

    update models

--- Server/src/app/models/user.model.js
+++ Server/src/app/models/user.model.js
@@ -3,9 +3,13 @@ const Schema = mongoose.Schema;
 
 const User = new Schema({
   name: { type: String, require: true },
-  sdt: { type: String, require: true },
+  phoneNumber: { type: String, require: true },
   address: { type: String },
-  isAdmin: { type: Boolean, require: true },
+  role: { type: String, enum: ['customer', 'admin'], default: 'customer' },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  registrationDate: { type: Date, default: Date.now },
+  profileImage: { type: String },
 });
 
 module.exports = mongoose.model('Users', User);
