85ea317ae53e1b86a06e5c573860dc9d28c5b418
Wunnn309 <minhquan300902@gmail.com>
2024-03-29 04:00:01 +0700

    quan

--- Client/src/Pages/Login.jsx
+++ Client/src/Pages/Login.jsx
@@ -1,20 +1,65 @@
-import React from 'react'
-import './CSS/Login.css'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { useState } from 'react';
+import './CSS/Login.css';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3000/user/login',{
+      email,
+      password,
+      }); // Make an API request
+      console.log(response.data); // Log the response data
+      if (email.includes('@admin')) {
+        // Chuyển hướng đến trang admin
+        navigate('/admin');
+      } else {
+        // Chuyển hướng đến trang chủ bình thường
+        navigate('/');
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error.message);
+    }
+  };
   return (
-    <div className='login'>
-      <div className='login-container'>
+    <div className="login">
+      <div className="login-container">
         <h1>Đăng Nhập</h1>
-        <div className='login-fields'>
-          <input type='text' placeholder='Nhập Tên Tài Khoản...' />
-          <input type='password' placeholder='Nhập Mật Khẩu...' />
+        <form onSubmit={handleSubmit}>
+          <div className="login-fields">
+            <input 
+              type="text"
+              name='email'
+              placeholder="Nhập Email..." 
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password" 
+              name='password' 
+              placeholder="Nhập Mật Khẩu..." 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+            />
           </div>
           <button>Đăng Nhập</button>
-          <p className='login-login'>Bạn chưa có tài khoản?<Link style={{ textDecoration: 'none'}} to ='/signup'><span>    Đăng ký ngay</span></Link></p>
+        </form>
+        <p className="login-login">
+          Bạn chưa có tài khoản?
+          <Link style={{ textDecoration: 'none' }} to="/user/signup">
+            <span> Đăng ký ngay</span>
+          </Link>
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
