66e1a33f105f6997ff5c8fa384295f52fdbd0128
Nguyen Thanh Danh <nguyenthanhdanh221102@gmail.com>
2024-04-16 21:10:42 +0700

    them {} vào book.controller

--- Server/src/app/controllers/book.controller.js
+++ Server/src/app/controllers/book.controller.js
@@ -4,7 +4,7 @@ const { mongooseToObject } = require('../../util/mongoose');
 class BookController {
   async all(req, res, next) {
     try {
-      const books = await Book.find();
+      const books = await Book.find({});
       console.log('All Book Fetched');
       res.send(books);
     } catch (error) {
