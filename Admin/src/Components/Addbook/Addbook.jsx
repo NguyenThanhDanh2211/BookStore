d1ff60143b4277c1b8f0693b17ed556985625ba1
Nguyễn Thành Danh <118902779+NguyenThanhDanh2211@users.noreply.github.com>
2024-04-16 23:38:15 +0700

    Update Addbook.jsx

--- Admin/src/Components/Addbook/Addbook.jsx
+++ Admin/src/Components/Addbook/Addbook.jsx
@@ -11,7 +11,7 @@ const Addbook = () => {
     price: '',
     description: '',
     image: '',
-    category: 'Sách Việt Nam',
+    category: 'in',
     discount: '',
   });
   const imageHandler = (e) => {
