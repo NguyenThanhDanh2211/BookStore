28060b4bf7c0a4f0eeebc21a83e99617ad611f4a
Nguyen Thanh Danh <nguyenthanhdanh221102@gmail.com>
2024-04-17 20:51:43 +0700

    update recommed, link đến cái sách được gợi ý

--- Recommender/app.py
+++ Recommender/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 import pickle
 import numpy as np
 
-from flask_cors import CORS  # Import thêm Flask CORS
+from flask_cors import CORS  
 
 app = Flask(__name__)
 CORS(app)  # Kích hoạt Flask CORS
@@ -15,14 +15,17 @@ book_pivot = pickle.load(open('./artifacts/book_pivot.pkl','rb'))
 
 def fetch_poster(suggestion):
     poster_urls = []
+    bookIds = []
     for book_id in suggestion:
         book_name = book_pivot.index[book_id]
         # Tìm index của sách trong DataFrame final_rating
         idx = np.where(final_rating['title'] == book_name)[0][0]
-        # Lấy URL của poster từ DataFrame final_rating
+        # Lấy URL của poster, lấy ISBN của sách từ DataFrame final_rating
         poster_url = final_rating.iloc[idx]['img_url']
+        bookId = final_rating.iloc[idx]['ISBN']
+        bookIds.append(bookId)
         poster_urls.append(poster_url)
-    return poster_urls
+    return poster_urls, bookIds
 
 def recommend_book(book_name):
     books_list = []
@@ -30,14 +33,14 @@ def recommend_book(book_name):
     # Sử dụng model để tìm các sách được đề xuất
     distance, suggestion = model.kneighbors(book_pivot.iloc[book_id,:].values.reshape(1,-1), n_neighbors=6)
     
-    # Lấy URL của poster cho các sách được đề xuất
-    poster_urls = fetch_poster(suggestion[0])
-    
+    # Lấy URL của poster, ISBN cho các sách được đề xuất
+    poster_urls, bookIds = fetch_poster(suggestion[0])
+
     for i in range(len(suggestion[0])):
         books_list.append(book_pivot.index[suggestion[0][i]])
-    return books_list, poster_urls
+    return books_list, poster_urls, bookIds
 
-@app.route('/recommend', methods=['GET', 'POST'])  # Thêm phương thức POST
+@app.route('/recommend', methods=['GET', 'POST'])  
 def get_recommendation():
     if request.method == 'POST':
         data = request.get_json()
@@ -45,11 +48,12 @@ def get_recommendation():
         if not selected_book:
             return jsonify({'error': 'No book selected'}), 400
         
-        recommended_books, poster_urls = recommend_book(selected_book)
+        recommended_books, poster_urls, bookIds = recommend_book(selected_book)
 
-        return jsonify({'recommended_books': recommended_books, 'poster_urls': poster_urls})
+        return jsonify({'recommended_books': recommended_books, 'poster_urls': poster_urls, 'bookIds': bookIds})
     else:
         return jsonify({'error': 'Method not allowed'}), 405
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)  
