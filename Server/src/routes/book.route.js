1a50066a8f4b8097c1b4d4c0ceb3b915d43bacf9
Nguyen Thanh Danh <nguyenthanhdanh221102@gmail.com>
2024-04-18 16:05:28 +0700

    Search

--- Server/src/routes/book.route.js
+++ Server/src/routes/book.route.js
@@ -5,7 +5,7 @@ const bookController = require('../app/controllers/book.controller');
 router.get('/getall', bookController.all);
 router.get('/getcategory', bookController.category);
 
+router.get('/filter', bookController.filter);
 router.get('/:id', bookController.show);
-router.get('/description/:name', bookController.description);
 
 module.exports = router;
