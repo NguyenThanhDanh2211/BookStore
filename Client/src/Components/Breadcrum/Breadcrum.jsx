8b408834331f6b45bd768c990ff8fa6a304cd24e
Nguyen Thanh Danh <nguyenthanhdanh221102@gmail.com>
2024-04-21 00:52:03 +0700

    fix giao dien

--- Client/src/Components/Breadcrum/Breadcrum.jsx
+++ Client/src/Components/Breadcrum/Breadcrum.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import thư viện Link từ react-router-dom
 import './Breadcrum.css';
 import arrow from '../Assets/arrow.png';
 
@@ -6,10 +7,16 @@ const Breadcrum = (props) => {
   const { book } = props;
   return (
     <div className="breadcrum">
-      Home <img className="img" src={arrow} alt="mui ten" /> BOOK{' '}
-      <img className="img" src={arrow} alt="" /> {props.category}{' '}
-      <img className="img" src={arrow} alt="" />
-      {props.name}
+      <Link to="/">Home</Link> {/* Thêm Link cho Home */}
+      <img className="img-muiten" src={arrow} alt="mui ten" />
+      {book.category === 'in' ? (
+        <Link to="/in">Sách Việt Nam</Link>
+      ) : (
+        <Link to="/on">Sách nước ngoài</Link>
+      )}
+      <img className="img-muiten" src={arrow} alt="" />
+      <span>{book.name}</span>{' '}
+      {/* Đổi thành span để hiển thị text không được link */}
     </div>
   );
 };
--- Client/src/Components/Description/Description.jsx
+++ Client/src/Components/Description/Description.jsx
@@ -1,14 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './Description.css';
 
 const Description = (props) => {
   const { book } = props;
   return (
     <div className="descriptionbox">
-      <div className="descriptionbox-nav">
-        <div className="descriptionbox-nav-box">Description</div>
-        <div className="descriptionbox-nav-box fade">Reviews</div>
+      <h3>Thông tin sản phẩm</h3>
+      <div className="descriptionbox-id">
+        Mã hàng: <b>{book.id}</b>
       </div>
+      <div className="descriptionbox-author">
+        Tác Giả: <b>{book.author}</b>
+      </div>
+      <div className="descriptionbox-nsx">
+        Nhà Xuất Bản: <b>{book.publisher}</b>
+      </div>
+      <div className="descriptionbox-cate">
+        Thể loại:{' '}
+        <b>{book.category === 'in' ? 'Sách Việt Nam' : 'Sách nước ngoài'}</b>
+      </div>
+      <p>
+        Giá sản phẩm trên SmartBook.com đã bao gồm thuế theo luật hiện hành. Bên
+        cạnh đó, tuỳ vào loại sản phẩm, hình thức và địa chỉ giao hàng mà có thể
+        phát sinh thêm chi phí khác như Phụ phí đóng gói, phí vận chuyển, phụ
+        phí hàng cồng kềnh,...
+      </p>
+      <p style={{ color: 'red' }}>
+        Chính sách khuyến mãi trên SmartBook.com không áp dụng cho Hệ thống Nhà
+        sách SmartBook trên toàn quốc.
+      </p>
+      <hr></hr>
+      <h3>{book.name}</h3>
       <div className="descriptionbox-description">
         <p>{book.description}</p>
       </div>
